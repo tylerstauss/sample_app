9f33e2edffacbb978190c60d15d56d58a4343aa8
Tyler Stauss <tyler.e.stauss@gmail.com>
2013-11-21 13:32:11 -0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,10 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the right title" do
+ 	  visit '/static_pages/home'
+ 	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+end
   end
 
   describe 'Help page' do
@@ -14,6 +18,11 @@ describe "Static pages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+
+  	it "should have the right title" do
+  visit '/static_pages/help'
+  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+end
   end
 
   describe 'About page' do
@@ -21,5 +30,11 @@ describe "Static pages" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
   	end
+  	it "should have the right title" do
+  visit '/static_pages/about'
+  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+end
   end
+
+
 end
