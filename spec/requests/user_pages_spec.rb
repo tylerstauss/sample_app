6bcc60dfe645d301d526689bd63d42576b028bc8
Tyler Stauss <tyler.e.stauss@gmail.com>
2013-11-25 15:59:37 -0800

    Finish user signup
    Finish user signup, add ssl and add error testing

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,7 +28,16 @@ require 'spec_helper'
   		it "should not create a user" do
   			expect { click_button submit }.not_to change(User, :count)
   		end
+
+  		describe "after submission" do
+  			before { click_button submit }
+
+  			it { should have_title('Sign up') }
+  			it { should have_content('error') }
+  		end
   	end
+
+  
 
   	describe "with valid information" do
   		before do
@@ -41,6 +50,15 @@ require 'spec_helper'
   		it "should create a user" do
   			expect { click_button submit }.to change(User, :count).by(1)
   		end
+
+  		describe "after saving the user" do
+  			before { click_button submit }
+  			let(:user) {User.find_by(email: 'user@example.com') }
+
+  			it { should have_title(user.name) }
+  			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+  		end
+
   		end
 	end
 end
