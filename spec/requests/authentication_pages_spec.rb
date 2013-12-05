1007668db4e6188464facb765ca4004781ea4937
Tyler Stauss <tyler.e.stauss@gmail.com>
2013-12-05 08:24:38 -0800

    Edit Gravatar Blank
    open edit gravatar in a  new tab

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,6 +9,8 @@ describe "Authentication" do
 
     		it { should have_title('Sign in') }
     		it { should have_content('Sign in') }
+    		it { should_not have_link('Profile') }
+    		it { should_not have_link('Settings')}
 
 
     	describe "with invalid information" do
